05b856e6d2b918c805c997503deae944b2b44c01
sancpjor <sancpjor@amazon.es>
2025-10-30 17:00:47 +0100

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,5 +1,8 @@
 const awsconfig = {
-  // Configuración de AWS Amplify (vacía por ahora)
+aws_project_region: 'eu-west-3',
+  aws_cognito_region: 'eu-west-3',
+  aws_user_pools_id: 'eu-west-3_lHUi9pWBS',
+  aws_user_pools_web_client_id: '5ih9lsr8cv6gpvlblpar1sndf3', 
 };
 
 export default awsconfig;
